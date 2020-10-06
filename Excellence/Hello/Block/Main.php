b0a8d9578e37b610b81f891c0f8887cbe3776805
Master <kamal@excellencetechnologies.info>
2020-10-06 13:15:26 +0530

    Removed redundant files of blog practice

--- Excellence/Hello/Block/Main.php
+++ Excellence/Hello/Block/Main.php
@@ -4,19 +4,16 @@
     class Main extends \Magento\Framework\View\Element\Template
     {   
         protected $_customerSession;
-        protected $_testFactory;
         protected $_logtestFactory;
 
         public function __construct(
             \Magento\Framework\View\Element\Template\Context $context,
-            \Excellence\Hello\Model\TestFactory $testFactory,
             \Excellence\Hello\Model\LogtestFactory $logtestFactory,
             \Magento\Customer\Model\SessionFactory $customerSession,
             array $data = []
         )
         {   
             $this->_customerSession = $customerSession->create();
-            $this->_testFactory = $testFactory;
             $this->_logtestFactory = $logtestFactory;
             parent::__construct($context, $data);
         }
--- Excellence/Hello/Setup/InstallSchema.php
+++ Excellence/Hello/Setup/InstallSchema.php
@@ -19,45 +19,10 @@ class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
         //START: install stuff
         //END:   install stuff
 
-        //START table setup
-        $table = $installer->getConnection()->newTable(
-            $installer->getTable('excellence_hello_test')
-        )->addColumn(
-            'excellence_hello_test_id',
-            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-            null,
-            [ 'identity' => true, 'nullable' => false, 'primary' => true, 'unsigned' => true ],
-            'Entity ID'
-        )->addColumn(
-            'title',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            255,
-            [ 'nullable' => false ],
-            'Demo Title'
-        )->addColumn(
-            'creation_time',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-            null,
-            [ 'nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT ],
-            'Creation Time'
-        )->addColumn(
-            'update_time',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-            null,
-            [ 'nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE ],
-            'Modification Time'
-        )->addColumn(
-            'is_active',
-            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
-            null,
-            [ 'nullable' => false, 'default' => '1' ],
-            'Is Active'
-        );
-
         $table1 = $installer->getConnection()->newTable(
             $installer->getTable('logdata')
         )->addColumn(
-            'id',
+            'logdata_id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
             [ 'identity' => true, 'nullable' => false, 'primary' => true, 'unsigned' => true ]
@@ -77,7 +42,6 @@ class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             null
         );
-        $installer->getConnection()->createTable($table);
         $installer->getConnection()->createTable($table1);
         //END   table setup
         $installer->endSetup();
