e99ef25e07a1ca4203ae002205e91e92c168cb80
Master <kamal@excellencetechnologies.info>
2020-10-07 18:35:27 +0530

    Resolved the issue with mutiple logout

--- Excellence/Hello/Observer/Logout/Time.php
+++ Excellence/Hello/Observer/Logout/Time.php
@@ -8,6 +8,8 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Excellence\Hello\Model\LogtestFactory;
 use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Customer\Model\SessionFactory;
+ 
  
  
 class Time implements ObserverInterface
@@ -15,26 +17,24 @@ class Time implements ObserverInterface
     protected $_date;
     protected $logger;
     protected $_logtestFactory;
+    protected $_customerSession;
  
    
-    public function __construct(LoggerInterface $logger, LogtestFactory $logtestFactory, DateTime $date)
+    public function __construct(LoggerInterface $logger, LogtestFactory $logtestFactory, DateTime $date, SessionFactory $customerSession)
     {
         $this->logger = $logger;
         $this->_date = $date;
         $this->_logtestFactory = $logtestFactory;
+        $this->_customerSession = $customerSession->create();
     }
  
-    public function execute(Observer $observer)
-    {
+    public function execute(Observer $observer) 
+    {   
+        $custmail = $this->_customerSession->getCustomerData()->getEmail();
         $date = $this->_date->gmtDate();
-        $logdata=$this->_logtestFactory->create()->getCollection()->getData();
-        $col = array_reverse($logdata);
-        $lastID = $col[0]['logdata_id'];
-        $collections = $this->_logtestFactory->create()->getCollection()->addFieldToFilter('logdata_id', array('eq' => $lastID));
-        foreach($collections as $item)
-        {
-            $item->setLogout( $date);
-        }
+        $collections = $this->_logtestFactory->create()->getCollection()->addFieldToFilter('email', $custmail)->addFieldToFilter('logout', array('null' => true));
+        $data = $collections->getFirstItem();
+        $data->setLogout( $date);
         $collections->save();
     }
 }
