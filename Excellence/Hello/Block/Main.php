8fa36066802e89d0b7c6767f241e6e63ef96e9fa
Master <kamal@excellencetechnologies.info>
2020-10-07 14:36:32 +0530

    reverted changes

--- Excellence/Hello/Block/Main.php
+++ Excellence/Hello/Block/Main.php
@@ -15,40 +15,10 @@
             parent::__construct($context, $data);
         }
 
-
         protected function _prepareLayout()
-        {
-            
-            $this->pageConfig->getTitle()->set(__('Login Logout Data'));
-            if ($this->getLogHistory()) {
-                $pager = $this->getLayout()->createBlock(
-                    'Magento\Theme\Block\Html\Pager',
-                    'reward.history.pager'
-                )->setAvailableLimit(array(5=>5,10=>10,15=>15,20=>20))
-                    ->setShowPerPage(true)->setCollection(
-                    $this->getLogHistory()
-                );
-                $this->setChild('pager', $pager);
-                $this->getLogHistory()->load();
-            }
-            return $this;
-        }
-
-        public function getPagerHtml()
-        {
-            return $this->getChildHtml('pager');
-        }
-
-        Public function getLogHistory()
-        {
-            $page=($this->getRequest()->getParam('p'))? $this->getRequest()->getParam('p') : 1;
-            $pageSize=($this->getRequest()->getParam('limit'))? $this->getRequest
-            ()->getParam('limit') : 5;
-            
-            $collection = $this->_logtestFactory->create()->getCollection();
-            $collection->setPageSize($pageSize);
-            $collection->setCurPage($page);
-            return $collection;
+        {   
+            $logdata=$this->_logtestFactory->create()->getCollection()->getData();
+            $this->setText($logdata);
         }
     }
 
